9c254d89cd3c1bda670fd69b0123749e3fd2273a
beogenius <70867972+beogenius@users.noreply.github.com>
2020-10-05 14:03:47 +0700

    Values

---
+++ src/Values.java
@@ -0,0 +1,18 @@
+public class Values {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5357f;
+        double d = 20.598607980976869809809809877687;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
